73d19b15b83866c4ba4cbed61f93b913f7330d70
Tommie <gabbaltazar23@gmail.com>
2025-08-13 09:48:18 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $color = "red";
+
+    echo "My name is Gabrielle Rae M. Baltazar " . $color . "<br>";
+
+    echo "My age is 19 " . $color . "<br>";
+
+    echo "I'm a 2nd year college of Global Reciprocal Colleges " . $color . "<br>";
+
+    echo "Bachelor of Science in Information Technology " . $color . "<br>";
+
+    echo "Websys " . $color . "<br>";
+
+    $c = true;  // 
+    echo "c is " . (is_bool($c) ? 'true' : 'false') . "<br>";  // 
+
+    $d = false; // 
+    echo "d is " . (is_bool($d) ? 'true' : 'false') . "<br>";  //
+
+?>
