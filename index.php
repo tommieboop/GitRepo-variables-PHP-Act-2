a5b11f2a45a6ffe7e826e4a3857852e495748394
Tommie <gabbaltazar23@gmail.com>
2025-08-13 22:37:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
-    $color = "red";
+    // $color = "red"; // You can delete or comment this line if no longer needed
 
-    echo "My name is Gabrielle Rae M. Baltazar " . $color . "<br>";
+    echo "My name is Gabrielle Rae M. Baltazar<br>";
 
-    echo "My age is 19 " . $color . "<br>";
+    echo "My age is 19<br>";
 
-    echo "I'm a 2nd year college of Global Reciprocal Colleges " . $color . "<br>";
+    echo "I'm a 2nd year college of Global Reciprocal Colleges<br>";
 
-    echo "Bachelor of Science in Information Technology " . $color . "<br>";
+    echo "Bachelor of Science in Information Technology<br>";
 
-    echo "Websys " . $color . "<br>";
+    echo "Websys<br>";
 
-    $c = true;  // 
-    echo "c is " . (is_bool($c) ? 'true' : 'false') . "<br>";  // 
+    $c = true;
+    echo "c is " . (is_bool($c) ? 'true' : 'false') . "<br>";
 
-    $d = false; // 
-    echo "d is " . (is_bool($d) ? 'true' : 'false') . "<br>";  //
-
+    $d = false;
+    echo "d is " . (is_bool($d) ? 'true' : 'false') . "<br>";
 ?>
